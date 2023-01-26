b949a2f3e259d4bae7d716bfe6e9bcac41bf741e
Vishal Kumar <klv020439@gmail.com>
2023-01-26 20:25:10 +0530

    Adding basic bit operations files

---
+++ Bit_manipulation/Basic_bit_operation/Basic_bit.cpp
@@ -0,0 +1,19 @@
+// Progrm to show basic bit manipulation operations like (&, |, ~, ^, <<, >>)
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Bitwise AND(&): 5 & 2 = " << (5 & 2) << endl; // 0
+
+    cout << "Bitwise OR(|): 5 | 2 = " << (5 | 2) << endl; // 7
+
+    cout << "Bitwise NOT(~): ~5 = " << (~5) << endl; // -6
+
+    cout << "Bitwise XOR(^): 5 ^ 3 = " << (5 ^ 3) << endl; // 6
+
+    cout << "Left Shift(<<): 5 << 1 = " << (5 << 1) << endl; // 10
+
+    cout << "Right Shift(>>): 5 >> 1 = " << (5 >> 1) << endl; // 2
+
+}
